1f751b2d78bf15915a7b0ea2585441ac9c8fcd1f
Jeff Sharp <jeff.sharp@git>
2018-01-28 11:10:29 -0800

    Add ServiceCore project. This eventually can be its own NuGet package.

---
+++ ServiceCore/Properties/AssemblyInfo.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security;
+
+// General Information
+[assembly: AssemblyTitle       ("ServiceCore")]
+[assembly: AssemblyDescription ("Windows service host and installer")]
+[assembly: AssemblyProduct     ("ServiceCore")]
+[assembly: AssemblyCompany     ("(to be determined)")]
+[assembly: AssemblyCopyright   ("Copyright 2018 (to be determined)")]
+[assembly: AssemblyVersion     ("0.0.0")]
+
+// Compliance
+[assembly: ComVisible(false)]
+
+// Security
+[assembly: SecurityRules(SecurityRuleSet.Level2)]
+[assembly: InternalsVisibleTo("ServiceCore.Tests")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
+                            // ^^^^^^^^^^^^^^^^^^^^^^^^
+                            // Required for Moq to mock a class with an internal abstract method.
+
+// Configuration
+#if DEBUG
+    [assembly: AssemblyConfiguration("Debug")]
+#else
+    [assembly: AssemblyConfiguration("Release")]
+#endif

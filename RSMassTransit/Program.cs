988d1bb4237cae597d2980f8ba999c4b7901f346
Jeff Sharp <jeff.sharp@git>
2018-01-28 10:51:18 -0800

    Add RSMassTransit project.

---
+++ RSMassTransit/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace RSMassTransit
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ RSMassTransit/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+
+// General Information
+[assembly: AssemblyTitle       ("RSMassTransit")]
+[assembly: AssemblyDescription ("MassTransit message bus interface for SQL Server Reporting Services")]
+
+// Security
+//
+// All types and members are security-critical, except where
+// being security-critical violates an inheritance rule.
+// http://msdn.microsoft.com/en-us/library/dd233102.aspx
+//
+// (default behavior; no security attribute)

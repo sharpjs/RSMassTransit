2d84a728192854f95f43a438b983d394404533b6
Jeff Sharp <jeff.sharp@git>
2018-01-29 10:52:02 -0800

    Add RSMassTransit.Messages project.

---
+++ RSMassTransit.Messages/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Security;
+
+// General Information
+[assembly: AssemblyTitle       ("RSMassTransit.Messages")]
+[assembly: AssemblyDescription ("MassTransit message types for SQL Server Reporting Services")]
+
+// Security
+//
+// All code is transparent.
+// The entire assembly will not do anything privileged or unsafe.
+// http://msdn.microsoft.com/en-us/library/dd233102.aspx
+//
+[assembly: SecurityTransparent]

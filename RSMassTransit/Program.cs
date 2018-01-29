215f7aaae0e273e411039976c4112f65cbe84375
Jeff Sharp <jeff.sharp@git>
2018-01-28 16:36:01 -0800

    Working test communication with local SSRS server.

--- RSMassTransit/Program.cs
+++ RSMassTransit/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net;
+using System.Security.Principal;
 
 namespace RSMassTransit
 {
@@ -6,6 +8,17 @@ namespace RSMassTransit
     {
         private static void Main(string[] args)
         {
+            using (var client = new ReportingServices.Execution.ReportExecutionServiceSoapClient())
+            {
+                var credential                       = client.ClientCredentials.Windows;
+                credential.AllowedImpersonationLevel = TokenImpersonationLevel.Impersonation;
+                credential.ClientCredential          = CredentialCache.DefaultNetworkCredentials;
+
+                client.ListSecureMethods(
+                    null,
+                    out string[] methods
+                );
+            }
         }
     }
 }

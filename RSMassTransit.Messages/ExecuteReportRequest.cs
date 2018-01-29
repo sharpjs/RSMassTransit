b15cfe875f6bfc8cb1e13cf7218c76aa0474c14f
Jeff Sharp <jeff.sharp@git>
2018-01-29 15:42:40 -0800

    Default implementations of report execution messages.

---
+++ RSMassTransit.Messages/ExecuteReportRequest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace RSMassTransit.Messages
+{
+    /// <summary>
+    ///   Default implementation of <see cref="IExecuteReportRequest"/>.
+    /// </summary>
+    public class ExecuteReportRequest : IExecuteReportRequest
+    {
+        /// <inheritdoc/>
+        public string Path { get; set; }
+
+        /// <inheritdoc/>
+        public IList<KeyValuePair<string, string>> ParameterValues
+        {
+            get => _parameterValues ?? (_parameterValues = new List<KeyValuePair<string, string>>());
+            set => _parameterValues = value;
+        }
+        private IList<KeyValuePair<string, string>> _parameterValues;
+
+        /// <inheritdoc/>
+        public string ParameterLanguage { get; set; }
+
+        /// <inheritdoc/>
+        public ReportFormat Format { get; set; }
+    }
+}
---
+++ RSMassTransit.Messages/ExecuteReportResponse.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace RSMassTransit.Messages
+{
+    /// <summary>
+    ///   Default implementation of <see cref="IExecuteReportResponse"/>.
+    /// </summary>
+    public class ExecuteReportResponse : IExecuteReportResponse
+    {
+        /// <inheritdoc/>
+        public bool Succeeded { get; set; }
+
+        /// <inheritdoc/>
+        public IList<string> Messages
+        {
+            get => _messages ?? (_messages = new List<string>());
+            set => _messages = value;
+        }
+        private IList<string> _messages;
+
+        /// <inheritdoc/>
+        public string Uri { get; set; }
+
+        /// <inheritdoc/>
+        public string ContentType { get; set; }
+
+        /// <inheritdoc/>
+        public string FileNameExtension { get; set; }
+
+        /// <inheritdoc/>
+        public long Length { get; set; }
+    }
+}

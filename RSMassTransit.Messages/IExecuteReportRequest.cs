6c741e51ee2e02c415af818f5a7a9a57a3ee257b
Jeff Sharp <jeff.sharp@git>
2018-01-29 15:20:57 -0800

    First crack at report execution request and response messages.

---
+++ RSMassTransit.Messages/IExecuteReportRequest.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace RSMassTransit.Messages
+{
+    /// <summary>
+    ///   A request to execute a report.
+    /// </summary>
+    public interface IExecuteReportRequest
+    {
+        /// <summary>
+        ///   Virtual path of the report on the report server.
+        ///   Example: <c>"/My Reports/Contoso/BalanceSheet"</c>
+        /// </summary>
+        string Path { get; set; }
+
+        /// <summary>
+        ///   Values for the report's parameters.
+        /// </summary>
+        /// <remarks>
+        ///   To provide multiple values for one parameter, populate this
+        ///   collection with multiple key-value pairs for that parameter name.
+        /// </remarks>
+        IList<KeyValuePair<string, string>> ParameterValues { get; set; }
+
+        /// <summary>
+        ///   Language and locale used to interpret parameter values such as
+        ///   dates and numbers.  Example: <c>"en-US"</c>
+        /// </summary>
+        string ParameterLanguage { get; set; } // e.g. 
+
+        /// <summary>
+        ///   Format in which to render the report.
+        /// </summary>
+        ReportFormat Format { get; set; }
+    }
+}
---
+++ RSMassTransit.Messages/IExecuteReportResponse.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace RSMassTransit.Messages
+{
+    /// <summary>
+    ///   The response to a request to execute a report.
+    /// </summary>
+    public interface IExecuteReportResponse
+    {
+        /// <summary>
+        ///   Whether the report execution succeeded.
+        /// </summary>
+        bool Succeeded { get; set; }
+
+        /// <summary>
+        ///   Diagnostic messages produced during report execution.
+        /// </summary>
+        IList<string> Messages { get; set; }
+
+        /// <summary>
+        ///   Location from which to fetch the rendered report.
+        /// </summary>
+        string Uri { get; set; }
+
+        /// <summary>
+        ///   Content type (MIME type) of the rendered report.
+        /// </summary>
+        string ContentType { get; set; }
+
+        /// <summary>
+        ///   File name extension of the rendered report.
+        /// </summary>
+        string FileNameExtension { get; set; }
+
+        /// <summary>
+        ///   Length of the rendered report, in bytes.
+        /// </summary>
+        long Length { get; set; }
+    }
+}

0606c0a933812a13b4d95073db1d3a6f8468b577
Jeff Sharp <jeff.sharp@git>
2018-01-29 15:32:06 -0800

    Add stub consumer for execute request.

---
+++ RSMassTransit/ExecuteReportConsumer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MassTransit;
+
+using RSMassTransit.Messages;
+
+namespace RSMassTransit
+{
+    internal class ExecuteReportConsumer : IConsumer<IExecuteReportRequest>
+    {
+        public Task Consume(ConsumeContext<IExecuteReportRequest> context)
+        {
+            //await context.RespondAsync<IExecuteReportResponse>(...);
+            return Task.CompletedTask;
+        }
+    }
+}

02fc7b5e05c677b9a136612dfafde51bde7cc878
Jeff Sharp <jeff.sharp@git>
2018-01-30 08:27:55 -0800

    Add Configuration properties for message bus settings.

--- RSMassTransit/Core/Configuration.cs
+++ RSMassTransit/Core/Configuration.cs
@@ -1,4 +1,6 @@
-﻿namespace RSMassTransit
+﻿using System.Configuration;
+
+namespace RSMassTransit
 {
     internal class Configuration
     {
@@ -7,6 +9,25 @@
         public static Configuration Current
             => _current ?? (_current = new Configuration());
 
-        public string InstanceId { get; } = "Default";
+        public Configuration()
+        {
+            var settings = ConfigurationManager.AppSettings;
+
+            InstanceId           = settings[nameof(InstanceId)]             ?? "Default";
+
+            MessageBusType       = settings[nameof(MessageBusType)]         ?? "RabbitMQ";
+            MessageBusHost       = settings[nameof(MessageBusHost)]         ?? "localhost";
+            MessageBusQueue      = settings[nameof(MessageBusQueue)]        ?? "reports";
+            MessageBusSecretName = settings[nameof(MessageBusSecretName)]   .NullIfEmpty();
+            MessageBusSecret     = settings[nameof(MessageBusSecret)]       .NullIfEmpty();
+        }
+
+        public string InstanceId             { get; private set; }
+
+        public string MessageBusType         { get; private set; }
+        public string MessageBusHost         { get; private set; }
+        public string MessageBusQueue        { get; private set; }
+        public string MessageBusSecretName   { get; private set; }
+        public string MessageBusSecret       { get; private set; }
     }
 }

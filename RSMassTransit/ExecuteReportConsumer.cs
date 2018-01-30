c299cf711b6ce9c518ccf92c6a7617f93c97d58b
Jeff Sharp <jeff.sharp@git>
2018-01-29 17:36:58 -0800

    Stub use of client factory and execution service in consumer.

--- RSMassTransit/ExecuteReportConsumer.cs
+++ RSMassTransit/ExecuteReportConsumer.cs
@@ -1,18 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Net;
 using System.Threading.Tasks;
 using MassTransit;
 
 using RSMassTransit.Messages;
+using RSMassTransit.ReportingServices;
+using RSMassTransit.ReportingServices.Execution;
 
 namespace RSMassTransit
 {
     internal class ExecuteReportConsumer : IConsumer<IExecuteReportRequest>
     {
+        private readonly IReportingServicesClientFactory _clientFactory;
+
+        public ExecuteReportConsumer(IReportingServicesClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory
+                ?? throw new ArgumentNullException(nameof(clientFactory));
+        }
+
         public async Task Consume(ConsumeContext<IExecuteReportRequest> context)
         {
             var request  = context.Message;
             var response = new ExecuteReportResponse();
+
+            var credential = new NetworkCredential(request.UserName, request.Password);
+
+            using (var client = _clientFactory.CreateExecutionClient(credential))
+            {
+                var loadReportResponse = await client.LoadReport2Async(
+                    new LoadReport2Request { Report = request.Path }
+                );
+            }
 
             await context.RespondAsync<IExecuteReportResponse>(response);
         }

716a06e0f9eed183e74b7f37b9bec4877c029115
Jeff Sharp <jeff.sharp@git>
2018-01-29 18:35:31 -0800

    Add Configuration class.

---
+++ RSMassTransit/Core/Configuration.cs
@@ -0,0 +1,12 @@
+﻿namespace RSMassTransit
+{
+    internal class Configuration
+    {
+        private static Configuration _current;
+
+        public static Configuration Current
+            => _current ?? (_current = new Configuration());
+
+        public string InstanceId { get; } = "Default";
+    }
+}

0f796251475b0b4e287343847cf2c0c7ac0a1694
Jeff Sharp <jeff.sharp@git>
2018-01-30 07:47:50 -0800

    Create/dispose container.  Stub start/stop bus.

--- RSMassTransit/Core/RSMassTransitService.cs
+++ RSMassTransit/Core/RSMassTransitService.cs
@@ -1,4 +1,6 @@
-﻿using Sharp.ServiceHost;
+﻿using Autofac;
+using MassTransit;
+using Sharp.ServiceHost;
 
 namespace RSMassTransit.Core
 {
@@ -9,17 +11,56 @@ namespace RSMassTransit.Core
             DisplayName = "SQL Server Reporting Services MassTransit Interface",
             Description = "Executes reports in response to messages received on a MassTransit message bus.";
 
+        private IContainer _container;
+
         public RSMassTransitService()
             : base(Name) { }
 
         protected override void StartCore()
         {
-            base.StartCore();
+            CreateContainer();
+            StartBus();
         }
 
         protected override void StopCore()
         {
-            base.StopCore();
+            StopBus();
+            DisposeContainer();
+        }
+
+        protected override void Dispose(bool managed)
+        {
+            if (managed)
+                DisposeContainer();
+
+            base.Dispose(managed);
+        }
+
+        private void CreateContainer()
+        {
+            Log.Verbose("Creating IoC container.");
+            var builder = new ContainerBuilder();
+            //builder.RegisterModule<MessageBusModule>();
+            _container = builder.Build();
+        }
+
+        private void DisposeContainer()
+        {
+            Log.Verbose("Disposing IoC container.");
+            _container?.Dispose();
+            _container = null;
+        }
+
+        private void StartBus()
+        {
+            Log.Verbose("Starting message bus.");
+            //_container.Resolve<IBusControl>().Start();
+        }
+
+        private void StopBus()
+        {
+            Log.Verbose("Stopping message bus.");
+            //_container.Resolve<IBusControl>().Stop();
         }
     }
 }

fe6c3243681b9d1d37de6bb5c07740d4b1b106d8
Jeff Sharp <jeff.sharp@git>
2018-01-31 09:30:21 -0800

    Stub Invoke-RsReport cmdlet.

---
+++ RSMassTransit.PowerShell/InvokeRsReportCmdlet.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSMassTransit.PowerShell
+{
+    [Cmdlet(VerbsLifecycle.Invoke, "RsReport")]
+    public class InvokeRsReportCmdlet : PSCmdlet, IDisposable
+    {
+        [Parameter(Position = 0, Mandatory = true)]
+        [ValidateNotNullOrEmpty]
+        public string Path { get; set; }
+
+        [Parameter(Position = 1, Mandatory = true)]
+        [ValidateSet("Word", "Excel", "PowerPoint", "Pdf")]
+        public string Format { get; set; }
+
+        [Parameter(Position = 2)]
+        [AllowNull, AllowEmptyCollection]
+        public Hashtable Parameters { get; set; }
+
+        [Parameter]
+        [Credential]
+        public PSCredential Credential { get; set; }
+
+        protected override void BeginProcessing()
+        {
+            base.BeginProcessing();
+        }
+
+        protected override void ProcessRecord()
+        {
+            base.ProcessRecord();
+        }
+
+        protected override void EndProcessing()
+        {
+            base.EndProcessing();
+        }
+
+        public void Dispose()
+        {
+            // Note: Simple dispose, because only managed resources here
+        }
+    }
+}

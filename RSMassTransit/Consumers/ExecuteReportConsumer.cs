4ca1a4009850a5d8660909d474fbaedd50094970
Jeff Sharp <jeff.sharp@git>
2018-01-31 08:20:01 -0800

    Report rendering happy-path (untested).

--- RSMassTransit/Consumers/ExecuteReportConsumer.cs
+++ RSMassTransit/Consumers/ExecuteReportConsumer.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 using System.Threading.Tasks;
 using MassTransit;
 
@@ -20,26 +23,84 @@ namespace RSMassTransit.Consumers
 
         public async Task Consume(ConsumeContext<IExecuteReportRequest> context)
         {
-            var request    = context.Message;
-            var response   = new ExecuteReportResponse();
+            var request  = context.Message;
+            var response = new ExecuteReportResponse();
+
+            var bytes = ExecuteReport(request, response);
+            // upload to azure
+
+            await context.RespondAsync<IExecuteReportResponse>(response);
+        }
+
+        private async Task<byte[]> ExecuteReport(IExecuteReportRequest request, IExecuteReportResponse response)
+        {
             var credential = request.GetNetworkCredential();
 
             using (var client = _services.CreateExecutionClient(credential))
             {
-                var loadReportResponse = await client.LoadReport2Async(
+                var loaded = await client.LoadReport2Async(
                     new LoadReport2Request { Report = request.Path }
                 );
 
+                var executionHeader = loaded.ExecutionHeader;
 
                 await client.SetExecutionParameters2Async(
                     new SetExecutionParameters2Request
                     {
-                        
+                        ExecutionHeader   = executionHeader,
+                        Parameters        = GetParameterValues   (request),
+                        ParameterLanguage = GetParameterLanguage (request),
                     }
                 );
-            }
 
-            await context.RespondAsync<IExecuteReportResponse>(response);
+                var rendered = await client.Render2Async(new Render2Request
+                {
+                    ExecutionHeader = executionHeader,
+                    Format          = GetFormat(request),
+                    PaginationMode  = PageCountMode.Actual
+                });
+
+                response.ContentType       = rendered.MimeType;
+                response.FileNameExtension = rendered.Extension;
+                response.Messages          = TranslateWarnings(rendered.Warnings);
+                //???                      = rendered.Encoding;
+
+                return rendered.Result;
+            }
         }
+
+        private static ParameterValue[] GetParameterValues(IExecuteReportRequest request)
+            => request.ParameterValues
+                .Select(p => new ParameterValue { Name = p.Key, Value = p.Value })
+                .ToArray();
+
+        private static string GetParameterLanguage(IExecuteReportRequest request)
+            => request.ParameterLanguage.NullIfEmpty()
+                ?? CultureInfo.CurrentCulture.Name; // Perhaps make configurable?
+
+        private static string GetFormat(IExecuteReportRequest request)
+            => FormatNames[request.Format]; // TODO: Handle not-found errors
+
+        private static readonly Dictionary<ReportFormat, string>
+            FormatNames = new Dictionary<ReportFormat, string>
+            {
+                [ReportFormat.Word]        = "WORDOPENXML",
+                [ReportFormat.WordLegacy]  = "WORD",
+                [ReportFormat.Excel]       = "EXCELOPENXML",
+                [ReportFormat.ExcelLegacy] = "EXCEL",
+                [ReportFormat.PowerPoint]  = "PPTX",
+                [ReportFormat.Pdf]         = "PDF",
+                [ReportFormat.Tiff]        = "IMAGE",
+                [ReportFormat.Html4]       = "HTML4.0",
+                [ReportFormat.Html5]       = "HTML5",
+                [ReportFormat.Mhtml]       = "MHTML",
+                [ReportFormat.Csv]         = "CSV",
+                [ReportFormat.Xml]         = "XML"
+            };
+
+        private IList<string> TranslateWarnings(Warning[] warnings)
+            => warnings
+                .Select(w => $"{w.Severity}: [{w.ObjectType}/{w.ObjectName}]: {w.Message} [{w.Code}]")
+                .ToList();
     }
 }
--- RSMassTransit/Core/Program.cs
+++ RSMassTransit/Core/Program.cs
@@ -7,41 +7,6 @@ namespace RSMassTransit.Core
         private static void Main(string[] args)
         {
             ServiceHost<RSMassTransitService>.Run(args);
-            /*
-            using (var client = new ReportExecutionServiceSoapClient())
-            {
-                var credential                       = client.ClientCredentials.Windows;
-                credential.AllowedImpersonationLevel = TokenImpersonationLevel.Impersonation;
-                credential.ClientCredential          = CredentialCache.DefaultNetworkCredentials;
-
-                var loadResponse = client.LoadReport2(new LoadReport2Request
-                {
-                    Report = "/My Reports/AP Reports/350-APReport",
-                });
-
-                var execution = loadResponse.ExecutionHeader;
-
-                client.SetExecutionParameters2(new SetExecutionParameters2Request
-                {
-                    ExecutionHeader = execution,
-                    Parameters = new[]
-                    {
-                        new ParameterValue { Name = "IsVendor",         Value = "True" },
-                        new ParameterValue { Name = "CompanySelection", Value = "1"    },
-                    },
-                    ParameterLanguage = "en-US"
-                });
-
-                var renderResponse = client.Render2(new Render2Request
-                {
-                    ExecutionHeader = execution,
-                    Format          = "ExcelOpenXML",
-                    PaginationMode  = PageCountMode.Estimate
-                });
-
-                var bytes = renderResponse.Result;
-            }
-            */
         }
     }
 }

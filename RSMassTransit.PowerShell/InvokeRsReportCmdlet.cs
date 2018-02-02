884c8a66d483497debd29940eb88b3d41afbce09
Jeff Sharp <jeff.sharp@git>
2018-02-01 16:11:07 -0800

    First try implementation of Invoke-RsReport.

--- RSMassTransit.PowerShell/InvokeRsReportCmdlet.cs
+++ RSMassTransit.PowerShell/InvokeRsReportCmdlet.cs
@@ -1,24 +1,21 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+using System.Globalization;
 using System.Management.Automation;
-using System.Text;
-using System.Threading.Tasks;
+using MassTransit;
+using RSMassTransit.Messages;
 
 namespace RSMassTransit.PowerShell
 {
     [Cmdlet(VerbsLifecycle.Invoke, "RsReport")]
-    public class InvokeRsReportCmdlet : PSCmdlet, IDisposable
+    public class InvokeRsReportCmdlet : RSMassTransitCmdlet
     {
         [Parameter(Position = 0, Mandatory = true)]
         [ValidateNotNullOrEmpty]
         public string Path { get; set; }
 
         [Parameter(Position = 1, Mandatory = true)]
-        [ValidateSet("Word", "Excel", "PowerPoint", "Pdf")]
-        public string Format { get; set; }
+        public ReportFormat Format { get; set; }
 
         [Parameter(Position = 2)]
         [AllowNull, AllowEmptyCollection]
@@ -26,26 +23,64 @@ namespace RSMassTransit.PowerShell
 
         [Parameter]
         [Credential]
-        public PSCredential Credential { get; set; }
+        public PSCredential RsCredential { get; set; } = PSCredential.Empty;
+
+        private IRequestClient<IExecuteReportRequest, IExecuteReportResponse> _client;
 
         protected override void BeginProcessing()
         {
-            base.BeginProcessing();
+            CreateBus();
+            CreateRequestClient(out _client);
         }
 
         protected override void ProcessRecord()
         {
-            base.ProcessRecord();
+            var credential = RsCredential.GetNetworkCredential();
+
+            var request = new ExecuteReportRequest
+            {
+                Path              = Path,
+                Format            = Format,
+                ParameterValues   = GetParameters(),
+                ParameterLanguage = CultureInfo.CurrentCulture.Name,
+                UserName          = credential.UserName,
+                Password          = credential.Password
+            };
+
+            var response = _client.Request(request);
+        }
+
+        private IList<KeyValuePair<string, string>> GetParameters()
+        {
+            var pairs = new List<KeyValuePair<string, string>>();
+
+            if (Parameters == null)
+                return pairs;
+
+            foreach (DictionaryEntry e in Parameters)
+            {
+                if (e.Value is ICollection values)
+                    foreach (object value in values)
+                        pairs.Add(CreateParameter(e.Key, value));
+                else
+                    pairs.Add(CreateParameter(e.Key, e.Value));
+            }
+
+            return pairs;
+        }
+
+        private KeyValuePair<string, string> CreateParameter(object key, object value)
+        {
+            return new KeyValuePair<string, string>
+            (
+                key  ?.ToString() ?? "",
+                value?.ToString() ?? ""
+            );
         }
 
         protected override void EndProcessing()
         {
-            base.EndProcessing();
-        }
-
-        public void Dispose()
-        {
-            // Note: Simple dispose, because only managed resources here
+            DisposeBus();
         }
     }
 }

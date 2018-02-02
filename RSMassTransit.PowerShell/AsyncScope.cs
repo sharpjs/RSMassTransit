692156df352d5b726ac48ad17e54c6f9fa0000c5
Jeff Sharp <jeff.sharp@git>
2018-02-02 11:26:37 -0800

    Add AsyncScope.

---
+++ RSMassTransit.PowerShell/AsyncScope.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+
+namespace RSMassTransit.PowerShell
+{
+    /// <summary>
+    ///   A scope that enables synchronous code to invoke <c>Task</c>-based
+    ///   asynchronous code.
+    /// </summary>
+    /// <remarks>
+    ///   <para>
+    ///     In some scenarios, a thread has a <c>SynchronizationContext</c> that
+    ///     causes awaits to resume on that same thread.  This causes a deadlock
+    ///     when synchronous code starts an an asynchronous <c>Task</c> and
+    ///     blocks until the <c>Task</c> completes.  The deadlock occurs because
+    ///     the <c>Task</c>'s awaits cannot resume on the blocked thread.
+    ///     Temporarily suppressing the <c>SynchronizationContext</c> causes
+    ///     awaits to resume on <c>ThreadPool</c> threads instead, avoiding the
+    ///     deadlock.
+    ///   </para>
+    /// </remarks>
+    public sealed class AsyncScope : IDisposable
+    {
+        private readonly SynchronizationContext _context;
+
+        public AsyncScope()
+        {
+            _context = SynchronizationContext.Current;
+            SynchronizationContext.SetSynchronizationContext(null);
+        }
+
+        void IDisposable.Dispose()
+        {
+            SynchronizationContext.SetSynchronizationContext(_context);
+        }
+    }
+}

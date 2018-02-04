0000924f99219b88c8075270125319e2e0ed6281
Jeff Sharp <jeff.sharp@git>
2018-02-03 17:20:30 -0800

    Add Autofac module for storage.

---
+++ RSMassTransit/Storage/StorageModule.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Autofac;
+
+namespace RSMassTransit.Storage
+{
+    internal class StorageModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder
+                .Register(CreateRepository)
+                .SingleInstance()
+                .As<IBlobRepository>();
+        }
+
+        private IBlobRepository CreateRepository(IComponentContext context)
+        {
+            var configuration = context.Resolve<IStorageConfiguration>();
+
+            switch (configuration.StorageType)
+            {
+                case StorageType.FileSystem:
+                    return new FileSystemRepository(
+                        configuration.FileSystemPath
+                    );
+
+                case StorageType.AzureStorageBlob:
+                    return new AzureStorageBlobRepository(
+                        configuration.AzureStorageConnectionString,
+                        configuration.AzureStorageContainer
+                    );
+
+                default:
+                    // Should be unreachable
+                    throw new NotSupportedException();
+            }
+        }
+    }
+}
